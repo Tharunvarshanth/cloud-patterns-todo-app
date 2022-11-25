acecc39e6b9802cd9e6de5b88db6db0ffc0cbe87
Tharun varshanth <tharunvar10@gmail.com>
2022-11-25 22:15:15 +0530

    db connected succsiefully

--- auth/app.js
+++ auth/app.js
@@ -7,7 +7,7 @@ const path = require("path");
 const { Client } = require("pg");
 
 const client = new Client({
-  host: "localhost",
+  host: "todo-app-cloud.cdle6ghs5azs.ap-south-1.rds.amazonaws.com",
   port: 5432,
   user: "postgres",
   password: "rootroot",
@@ -37,33 +37,6 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 router.post("/sign-up", signUp);
-
-/*var dataEmail = {
-    Name: "email",
-    Value: "nnirosh447@gmail.com",
-  };
-
-  var dataName = {
-    Name: "name",
-    Value: "Niroshan",
-  };
-  var attributeList = [];
-  var attributeEmail = new AmazonCognitoIdentity.CognitoUserAttribute(dataEmail);
-  var attributeName = new AmazonCognitoIdentity.CognitoUserAttribute(dataName);
-  attributeList.push(attributeEmail);
-  attributeList.push(attributeName);
-
-  userPool.signUp("nnirosh447@gmail.com", "todo@12345", attributeList, null, async function (err, result) {
-    if (err) {
-      console.log(err.message || JSON.stringify(err));
-      res.status(400).send({ err: err });
-      return;
-    }
-    var cognitoUser = result.user;
-    console.log("user name is " + cognitoUser.getUsername());
-    res.json({ res: result });
-  });
-  res.send({ res: "no-response" });*/
 
 router.get("/index", function (req, res) {
   res.send({});
